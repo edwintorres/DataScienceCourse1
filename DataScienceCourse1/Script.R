697733c5aa5206351bbfc9781b23c9e9ba691def
Edwin Torres <edwin.torres@nagnoi.com>
2018-07-20 06:11:30 -0400

    Ended the section 1 from the course.

--- DataScienceCourse1/Script.R
+++ DataScienceCourse1/Script.R
@@ -52,3 +52,71 @@ co2
 
 
 ##test for 
+
+x <- 3
+log(exp(x))
+
+library(dslabs)
+data(murders)
+class(murders)
+murders
+str(murders)
+murders$state
+murders$population
+    
+
+pop <- murders$population
+pop
+length(pop)
+class(murders$region)
+
+levels(murders$region)
+
+a <- murders$abb
+class(a)
+
+1:5
+
+# We extract the population like this:
+p <- murders$population
+
+# This is how we do the same with the square brackets:
+o <- murders[["population"]]
+
+# We can confirm these two are the same
+identical(o, p)
+
+# Use square brackets to extract `abb` from `murders` and assign it to b
+b <- murders[["abb"]]
+
+# Check if `a` and `b` are identical 
+identical(a, b)
+
+# -------------------------
+# We can see the class of the region variable using class
+class(murders$region)
+
+# Determine the number of regions included in this variable 
+length(levels(murders$region))
+
+# --------------------------------
+# Here is an example of what the table function does
+x <- c("a", "a", "b", "b", "b", "c")
+table(x)
+
+# Write one line of code to show the number of states per region
+table(murders$region)
+
+murders
+
+
+n <- c(1, 2, 3)
+na <- c("elem1", "elem2", "elem3")
+
+names(n) <- na
+
+n[c(2,1)]
+n[c("elem1","elem2")]
+1
+
+murders
